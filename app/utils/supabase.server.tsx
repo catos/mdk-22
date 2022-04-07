7735ffbef7138e7beaffea5d50bb18fd59554ba3
Cato Skogholt <cato@nhi.no>
2022-04-07 13:57:04 +0200

    cleanup

--- app/utils/supabase.server.tsx
+++ app/utils/supabase.server.tsx
@@ -1,6 +1,5 @@
 // TODO: this is not only DB, supabase does more...
 import { createClient } from "@supabase/supabase-js";
-import { getSession } from "./session.server";
 
 // see documention about using .env variables
 // https://remix.run/docs/en/v1/guides/envvars#server-environment-variables
@@ -11,19 +10,4 @@ if (!url || !key) {
   throw new Error("supabase url || key not found...")
 }
 
-// TODO: rename to just "supabase"
 export const supabase = createClient(url, key);
-
-/**
- *
- * @param {*} request
- * @returns
- */
-export const hasAuthSession = async (request: Request) => {
-  let session = await getSession(request.headers.get("Cookie"));
-
-  if (!session.has("access_token")) 
-    throw Error("No session");
-
-  supabase.auth.setAuth(session.get("access_token"));
-};
